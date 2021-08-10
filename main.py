50feb7f1e3c245434178fdd03f097ba66854f656
Boxersteavee <76684157+Boxersteavee@users.noreply.github.com>
2021-08-10 15:03:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,28 +10,31 @@ import random
 
 TOKEN = os.environ['TOKEN']
 
-client = commands.Bot(command_prefix = 'z.')
-
-@client.event
-async def on_ready():
-    await client.change_presence(activity=discord.Game(name="z.cmds for a list of commands."))
-    print('{0.user} is ready'.format(client))
     
-@client.event
+
+zulu = commands.Bot(command_prefix = 'z.')
+
+@zulu.event
+async def on_ready():
+    await zulu.change_presence(activity=discord.Game(name="z.cmds for a list of commands."))
+    print('{0.user} is ready'.format(zulu))
+    
+
+@zulu.event
 async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        NotFound = discord.Embed(
+     if isinstance(error, commands.CommandNotFound):
+      NotFound = discord.Embed(
             title = 'Command does not exist!',
             description = 'I did not recognize the command you sent. Was it a typo? Try z.cmds to see a list of all commands',
             colour = discord.Colour.blue()
 
-        )
-        NotFound.set_footer(text='Did you make a typo?')
-        NotFound.set_author(name='No such command')
+      )
+      NotFound.set_footer(text='Did you make a typo?')
+      NotFound.set_author(name='No such command')
 
 
-        await ctx.send(embed=NotFound)
-    if isinstance(error, commands.MissingPermissions):
+      await ctx.send(embed=NotFound)
+      if isinstance(error, commands.MissingPermissions):
         NoPerms = discord.Embed(
             title = 'No permissions!',
             description = 'You do not have permission to do that command!',
@@ -41,37 +44,37 @@ async def on_command_error(ctx, error):
         NoPerms.set_footer(text='You can\'t do that command')
         NoPerms.set_author(name='You need Permission to run that command.')
         await ctx.send(embed=NoPerms)
-    if isinstance(error, UnboundLocalError):
+      if isinstance(error, UnboundLocalError):
         return
-    else:
+      else:
         raise error
 
-@client.command(aliases=['commands', 'commandlist'])
-async def cmds(ctx):
-    helpembed = discord.Embed(
-      title = 'Commands List',
-      description = 'Here is a list of commands.',
-      colour = discord.Colour.blue()
+#embed for help command.
+helpembed = discord.Embed(
+  title = 'Commands List',
+  description = 'Here is a list of commands.',
+  colour = discord.Colour.blue()
     
   )
 
-    helpembed.set_footer(text='This is the best discord bot')
-    helpembed.set_author(name='ZuluBot commands:')
-    helpembed.add_field(name='Ping!', value='Shows the ping of the bot')
-
+helpembed.set_footer(text='This is the best discord bot')
+helpembed.set_author(name='ZuluBot commands:')
+helpembed.add_field(name='Ping!', value='Shows the ping of the bot')
+@zulu.command(aliases=['commands', 'commandlist'])
+async def cmds(ctx):
+    global helpembed
 
     await ctx.send(embed=helpembed)
 
 class MyHelp(commands.HelpCommand):
+   #code that makes z.help work
     async def send_bot_help(self, mapping):
-        channel = self.get_destination()
-        await channel.send("Please use z.cmds for the help command. It will be a while before i get this help command working as i would need to rewrite the whole bot \(over 150 lines!\). THANKS! :smiley:")
+        await self.context.send(embed=helpembed)
 
-client.help_command = MyHelp()
-
-
-@client.command()
+@zulu.command()
 async def ping(ctx):
-  await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+  await ctx.send(f'Pong! {round(zulu.latency * 1000)}ms')
     
-client.run(TOKEN)
+
+zulu.help_command = MyHelp()
+zulu.run(TOKEN)
