6b4235a5ca96b32106470ddb5a1dfcb868cf9b81
Boxersteavee <76684157+Boxersteavee@users.noreply.github.com>
2021-10-30 18:48:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,34 +24,33 @@ async def on_ready():
     print('{0.user} is ready'.format(zulu))
     
 
-# @zulu.event
-# async def on_command_error(ctx, error):
-#      if isinstance(error, commands.CommandNotFound):
-#       NotFound = discord.Embed(
-#             title = 'Command does not exist!',
-#             description = 'I did not recognize the command you sent. Was it a typo? Try z.cmds to see a list of all commands',
-#             colour = discord.Colour.blue()
+@zulu.event
+async def on_command_error(ctx, error):
+     if isinstance(error, commands.CommandNotFound):
+      NotFound = discord.Embed(
+            title = 'I use slash commands now',
+            description = 'I have evolved with discord, i no longer use prefix commands as discord have made a new fancy feature called slash commands, press the `/` key see!',
+            colour = discord.Colour.blue()
 
-#       )
-#       NotFound.set_footer(text='Did you make a typo?')
-#       NotFound.set_author(name='No such command')
+      )
+      NotFound.set_footer(text='I am the best discord bot, you cannot change my mind as i was coded to say this.')
 
 
-#       await ctx.send(embed=NotFound)
-#       if isinstance(error, commands.MissingPermissions):
-#         NoPerms = discord.Embed(
-#             title = 'No permissions!',
-#             description = 'You do not have permission to do that command!',
-#             colour = discord.Colour.red()
+      await ctx.send(embed=NotFound)
+      if isinstance(error, commands.MissingPermissions):
+        NoPerms = discord.Embed(
+            title = 'No permissions!',
+            description = 'You do not have permission to do that command!',
+            colour = discord.Colour.red()
 
-#         )
-#         NoPerms.set_footer(text='You can\'t do that command')
-#         NoPerms.set_author(name='You need Permission to run that command.')
-#         await ctx.send(embed=NoPerms)
-#       if isinstance(error, UnboundLocalError):
-#         return
-#       else:
-#         raise error
+        )
+        NoPerms.set_footer(text='You can\'t do that command')
+        NoPerms.set_author(name='You need Permission to run that command.')
+        await ctx.send(embed=NoPerms)
+      if isinstance(error, UnboundLocalError):
+        return
+      else:
+        raise error
 
 #embed for help command.
 helpembed = discord.Embed(
@@ -66,18 +65,18 @@ helpembed.set_author(name='ZuluBot commands:')
 helpembed.add_field(name='Ping!', value='Shows the ping of the bot')
 helpembed.add_field(name='Help', value='Shows this command.')
 
-@zulu.slash_command(guild_ids=[798180194049196032])  # create a slash command for the supplied guilds
+@zulu.slash_command(guild_ids=[798180194049196032, 764981968579461130])  # create a slash command for the supplied guilds
 async def help(ctx):
     """Sends the help command"""  # the command description can be supplied as the docstring
     await ctx.respond(f"Hello <@{ctx.author.id}>!,", embed=helpembed)
 
-@zulu.slash_command(guild_ids=[798180194049196032])  # create a slash command for the supplied guilds
+@zulu.slash_command(guild_ids=[798180194049196032, 764981968579461130])  # create a slash command for the supplied guilds
 async def hello(ctx):
     """Say hello to the bot"""  # the command description can be supplied as the docstring
     await ctx.respond(f"Hello {ctx.author}!")
 
 
-@zulu.slash_command(guild_ids=[798180194049196032])
+@zulu.slash_command(guild_ids=[798180194049196032, 764981968579461130])
 async def ping(ctx):
   pingtime = round(zulu.latency * 1000)
   pingembed = discord.Embed(
@@ -91,19 +90,16 @@ async def ping(ctx):
  
   await ctx.respond(embed=pingembed)
     
-@zulu.slash_command(guild_ids=[798180194049196032])
+@zulu.slash_command(guild_ids=[798180194049196032, 764981968579461130])
 async def srvstatus(
     ctx,
     ip: Option(str, "Enter the server IP", required=True),
 ):
     r = requests.get(f'https://api.mcsrvstat.us/2/{ip}')
-    p = r.json()
+    server_json = r.json()
 
-    srvrespond = discord.Embed(
-    title = f'Status of{ip}',
-    )
-    await ctx.respond(embed=srvrespond)
+    await ctx.respond(embed=temprespond)
 
 
 
-zulu.run(TOKEN)
+zulu.run("ODc0MDEyMDg5NzA0OTA2NzUz.YRAxMA.iu6ee1kre4GCqigvTTD1f2gCTgI")
